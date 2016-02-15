99b1bd144c08bfcaf259953fe096ea1ad0d2e405
Vaibhav Raut <v.u.raut10@gmail.com>
2016-02-15 10:07:06 +0530

    Update port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // By default it gets index.html
 /*app.get('/',function(req, res) {
@@ -20,3 +20,5 @@ app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function(){
 	console.log('express server started on ' + PORT)
 });
+
+
